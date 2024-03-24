9f49f1c6423a2910a5ca120b2df9bdea186ba1bc
Austin <austinsusanto@gmail.com>
2024-03-24 14:13:58 +0700

    Seeded student, courses, and student_courses table

---
+++ src/main/java/id/ac/ui/cs/advprog/tutorialprofiling/TutorialProfilingApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.tutorialprofiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialProfilingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialProfilingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/tutorialprofiling/TutorialProfilingApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.tutorialprofiling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialProfilingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
